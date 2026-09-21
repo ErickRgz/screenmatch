84bd6e59f8cbd44d6269dfe7b15034e21aa54936
Erick Rodrigues <erickmanoel333rbr@gmail.com>
2026-09-21 20:52:32 -0300

    feat: cria primeira versão do ScreenMatch

---
+++ src/br/com/curso/screenmatch/model/Filme.java
@@ -0,0 +1,16 @@
+package br.com.curso.screenmatch.model;
+
+public class Filme extends Titulo{
+
+    private String diretor;
+
+    // getters e setters
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
